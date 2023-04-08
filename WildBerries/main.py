29d17c5032c8d15481623cd628628f7eb3f130ca
KsQRooW <dimankochetov@yandex.ru>
2023-04-08 03:39:03 +0300

    Парсинг содержимого элементов HTML страницы переписан через bs4. Сокращено время с 18-20с до 6с.

--- WildBerries/main.py
+++ WildBerries/main.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import re
 from time import sleep, time
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -16,21 +17,19 @@ class WildBerries:
     domain_url: str = "https://www.wildberries.ru"
     search_input: (str, str) = (By.ID, 'searchInput')
     content: (str, str) = (By.CLASS_NAME, "main-page")
-    card: (str, str) = (By.CLASS_NAME, "product-card__main")
+    card_selen: (str, str) = (By.CLASS_NAME, "product-card__main")
+    card_bs: (str, str) = ("a", "product-card__main")
     tags: (str, str) = (By.CLASS_NAME, "search-tags__header")
-    product_img: (str, str) = (By.CLASS_NAME, 'product-card__img')
-    img_tag: (str, str) = (By.TAG_NAME, 'img')
-    product_prices: (str, str) = (By.CLASS_NAME, "product-card__price")
-    product_brand_name: (str, str) = (By.CLASS_NAME, 'brand-name')
-    product_goods_name: (str, str) = (By.CLASS_NAME, 'goods-name')
+    product_img: (str, str) = ("div", 'product-card__img')
+    product_prices: (str, str) = ("div", "product-card__price")
+    product_brand_name: (str, str) = ("span", 'brand-name')
+    product_goods_name: (str, str) = ("span", 'goods-name')
 
 
-def scroll_down(driver) -> None:
-    i = 0
+def scroll_down(driver):
     last_height = driver.execute_script('return document.body.scrollHeight')
     new_height = last_height + 1
     while last_height != new_height:
-        i += 1
         driver.execute_script(f'window.scrollTo({{top: {last_height}, left: 0, behavior: "smooth"}});')
         sleep(0.5)
         last_height, new_height = new_height, driver.execute_script('return document.body.scrollHeight')
@@ -45,38 +44,52 @@ def all_prices_parsing(text: str) -> tuple:
     return tuple(int(price) for price in no_spaces.split(split_symbol) if price)
 
 
-driver = webdriver.Chrome()
+def parse_soup_html(card_elems):
+    res = []
+    for card in card_elems:
+        prices = all_prices_parsing(card.find(*WildBerries.product_prices).text)
+        res.append({
+            "url": card.get('href'),
+            "img": card.find(*WildBerries.product_img).find('img').get('src'),
+            "current_price": min(prices),
+            "old_price": max(prices),
+            "brand_name": card.find(*WildBerries.product_brand_name).text,
+            "goods_name": card.find(*WildBerries.product_goods_name).text
+        })
+    return res
 
-start = time()
-driver.get(WildBerries.domain_url)
 
-WebDriverWait(driver, 10).until(ec.presence_of_element_located(WildBerries.content))
-elem = driver.find_element(*WildBerries.search_input)
+def main():
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=options)
 
-elem.clear()
-elem.send_keys(input_name + Keys.ENTER)
-WebDriverWait(driver, 10).until(ec.presence_of_element_located(WildBerries.card))
+    start = time()
+    driver.get(WildBerries.domain_url)
 
-scroll_down(driver)
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located(WildBerries.content))
+    elem = driver.find_element(*WildBerries.search_input)
 
-card_elems = driver.find_elements(*WildBerries.card)
+    elem.clear()
+    elem.send_keys(input_name + Keys.ENTER)
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located(WildBerries.card_selen))
 
-# # Для отладки
-# with open('html_test.html', 'w', encoding='utf-8') as file:
-#     file.write(BeautifulSoup(driver.page_source, 'lxml').prettify())
+    scroll_down(driver)
 
-res = []
-for card in card_elems:
-    prices = all_prices_parsing(card.find_element(*WildBerries.product_prices).text)
-    res.append({
-        "url": card.get_attribute('href'),
-        "img": card.find_element(*WildBerries.product_img).find_element(*WildBerries.img_tag).get_attribute('src'),
-        "current_price": min(prices),
-        "old_price": max(prices),
-        "brand_name": card.find_element(*WildBerries.product_brand_name).text,
-        "goods_name": card.find_element(*WildBerries.product_goods_name).text
-    })
+    html_soup = BeautifulSoup(driver.page_source, features="lxml")
+    card_elems = html_soup.find_all(*WildBerries.card_bs)
 
-driver.close()
-pprint(res)
-print(time() - start)  # текущий результат: ~18-20 секунд
+    # # Для отладки
+    # with open('html_test.html', 'w', encoding='utf-8') as file:
+    #     file.write(BeautifulSoup(driver.page_source, 'lxml').prettify())
+
+    res = parse_soup_html(card_elems)
+
+    driver.close()
+    pprint(res)
+    print(len(res))
+    print(time() - start)  # текущий результат: ~6 секунд
+
+
+if __name__ == '__main__':
+    main()
